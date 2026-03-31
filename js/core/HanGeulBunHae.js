15e13b753f431956ec8839b8aa301685031c078d
qaoechi <junseong10683@gmail.com>
2026-03-31 14:52:40 +0900

    fix: Correct typo

--- js/core/HanGeulBunHae.js
+++ js/core/HanGeulBunHae.js
@@ -1,6 +1,6 @@
 import { HANGEUL_CHOSEONG, HANGEUL_JUNGSEONG, HANGEUL_JONGSEONG, SSANGJAEUM, GYEOPBATCHIM } from "../constant/hangeul/hangeul-mukja.js";
 
-export const HanGeulBunHae = (char) => {
+export const hanGeulBunHae = (char) => {
     const base = char.charCodeAt(0) - 0xAC00;
     if (base < 0 || base >= 0x2BA3) return null;
 
