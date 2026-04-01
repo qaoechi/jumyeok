558aca65dd71a832326b403e04483d714adee62d
qaoechi <junseong10683@gmail.com>
2026-04-01 16:48:25 +0900

    refactor: Change output to write to file for UTF-8 display

--- java/demo/src/main/java/com/junyeok/Main.java
+++ java/demo/src/main/java/com/junyeok/Main.java
@@ -1,30 +1,41 @@
 package com.junyeok;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.junyeok.model.Geul;
+import com.junyeok.service.jungja.HanGeulStrategy;
+import com.junyeok.service.jungja.JunJaStrategy;
 import com.junyeok.service.tokenize.GeulTypeResolver;
 
 public class Main {
     public static void main(String[] args) {
+        List<JunJaStrategy> jungja = List.of(new HanGeulStrategy());
         List<String> input = new ArrayList<>();
         input.add("as\nd");
         input.add("쀍");
         input.add(" 강의실");
-
         
         List<Geul> result = input.stream()
             .flatMap(in -> in.chars().mapToObj(c -> (char) c))
             .map(c -> GeulTypeResolver.resolve(c).create(c))
             .toList();
 
-
-        // result.stream()
-        //     .map(Geul::render)
-        //     .forEach(System.out::println);
-        result.stream()
-            .map(Geul::toString)
-            .forEach(System.out::println);
+        try (Writer writer = new BufferedWriter(
+            new OutputStreamWriter(new FileOutputStream("java/demo/target/ouput.txt"), StandardCharsets.UTF_8)
+        )) {
+            writer.write("a");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
