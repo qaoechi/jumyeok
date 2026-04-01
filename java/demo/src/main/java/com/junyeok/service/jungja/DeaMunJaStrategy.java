95469798530aa62a5ddba9daade17311f48fbdd9
qaoechi <junseong10683@gmail.com>
2026-04-01 22:32:34 +0900

    refactor: Seperate YeongEo mapping

---
+++ java/demo/src/main/java/com/junyeok/service/jungja/DeaMunJaStrategy.java
@@ -0,0 +1,17 @@
+package com.junyeok.service.jungja;
+
+import com.junyeok.constants.otherBraille.SoMunJa;
+import com.junyeok.model.Geul;
+import com.junyeok.model.braille.BrailleToken;
+import com.junyeok.model.braille.TokenType;
+
+public class DeaMunJaStrategy implements JunJaStrategy {
+
+    @Override
+    public BrailleToken uncontracted(Geul geul) {
+        return new BrailleToken(
+            String.valueOf(SoMunJa.fromChar(Character.toLowerCase(geul.getChar())).getBraille()),
+            TokenType.SOMUNJA,
+            geul);
+    }
+}
