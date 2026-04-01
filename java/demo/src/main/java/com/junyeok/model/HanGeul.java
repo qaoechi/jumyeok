8071e72cab4f025ac27be88df6cea874128ddd41
qaoechi <junseong10683@gmail.com>
2026-04-01 13:38:52 +0900

    refactor: Change jungseong type from char to String

--- java/demo/src/main/java/com/junyeok/model/HanGeul.java
+++ java/demo/src/main/java/com/junyeok/model/HanGeul.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public class HanGeul implements Geul {
     private final char doensori;
     private final char choseong;
-    private final char jungseong;
+    private final String jungseong;
     private final char jongseong;
     private final char gyeopbatchim;
 
