23b04fa5f90d41a166ded0a2f3ee4c0621d7bf50
qaoechi <junseong10683@gmail.com>
2026-04-15 19:48:44 +0900

    feat: Add other rules skeleton

---
+++ java/demo/src/main/java/com/junyeok/service/rule/YeongeoRule.java
@@ -0,0 +1,11 @@
+package com.junyeok.service.rule;
+
+import java.util.List;
+
+import com.junyeok.model.braille.BrailleToken;
+
+public class YeongeoRule {
+    public static void apply(List<BrailleToken> buffer, List<BrailleToken> result) {
+        result.addAll(buffer);
+    }
+}
