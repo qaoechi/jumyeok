4a105102c9c4e4dd043b84b57ae17df293488c32
qaoechi <junseong10683@gmail.com>
2026-04-15 17:45:42 +0900

    refactor: Change iteration to forward lookhead from 0

--- java/demo/src/main/java/com/junyeok/service/service/HangeulRule.java
+++ java/demo/src/main/java/com/junyeok/service/service/HangeulRule.java
@@ -10,30 +10,28 @@ import com.junyeok.model.mukja.Hangeul;
 
 public class HangeulRule {
     public static void apply(List<BrailleToken> buffer, List<BrailleToken> result) {
-        result.add(buffer.get(0));
-        for (int i = 1; i < buffer.size(); i++) {
-            Hangeul target = (Hangeul)buffer.get(i).getOrigin();
+        for (int i = 0; i < buffer.size() - 1; i++) {
+            Hangeul next = (Hangeul)buffer.get(i + 1).getOrigin();
             boolean flag = false;
-            if (target.getChoseong() == 'ㅇ') {
-                Hangeul prev = (Hangeul)buffer.get(i - 1).getOrigin();
-                if (target.getJungseong() == 'ㅖ') {
+            if (next.getChoseong() == 'ㅇ') {
+                Hangeul target = (Hangeul)buffer.get(i).getOrigin();
+                if (next.getJungseong() == 'ㅖ') {
                     flag = true;
-                } else if (JungseongAE.exists(prev.getJungseong()) && target.getJungseong() == 'ㅐ') {
+                } else if (JungseongAE.exists(target.getJungseong()) && next.getJungseong() == 'ㅐ') {
                     flag = true;
                 }
-                if (!flag || !(prev.getJongseong() == '\0' && prev.getGyeopbatchim() == '\0')) {
+                if (!flag || !(target.getJongseong() == '\0' && target.getGyeopbatchim() == '\0')) {
                     flag = false;
                 }
-
+                result.add(buffer.get(i));
                 if (flag) {
                     result.add(new BrailleToken(BrailleHelper.GUBUNPYO, TokenType.HELPER, null, false));
                 }
-                result.add(buffer.get(i));
             }
-            else if (buffer.get(i).isAbbreviation() && target.getJungseong() == '\0') {
-                
+            else if (buffer.get(i).isAbbreviation() && next.getChoseong() == 'ㅇ') {
+
             }
-            System.out.println(buffer.get(i).getOrigin());
         }
+        result.add(buffer.getLast());
     }
 }
