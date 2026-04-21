cae07799384dc2115ab78f494e9a84d1275fd674
qaoechi <junseong10683@gmail.com>
2026-04-22 00:50:45 +0900

    refactor: Change ouput stream logic

--- java/demo/src/main/java/com/junyeok/Main.java
+++ java/demo/src/main/java/com/junyeok/Main.java
@@ -10,9 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import com.junyeok.model.braille.BrailleToken;
 import com.junyeok.model.mukja.Buho;
 import com.junyeok.model.mukja.Geul;
 import com.junyeok.model.mukja.Gongbaek;
@@ -24,6 +22,7 @@ import com.junyeok.service.jungja.GongbaekStrategy;
 import com.junyeok.service.jungja.HangeulStrategy;
 import com.junyeok.service.jungja.JungjaStrategy;
 import com.junyeok.service.jungja.YeongeoStrategy;
+import com.junyeok.service.rule.BRFEngine;
 import com.junyeok.service.rule.RuleEngine;
 import com.junyeok.service.jungja.SutjaStrategy;
 import com.junyeok.service.tokenize.GeulTypeResolver;
@@ -53,44 +52,26 @@ public class Main {
         RuleEngine engine = new RuleEngine();
 
         List<String> input = new ArrayList<>();
-        input.add("혜당207 이공칠 학생쉼터→ 혜당207 이공칠 학생쉼터");
+        input.add("혜당207 이공칠렂 학생쉼터→ 혜당207 이공칠 학생쉼터");
+        input.add("아이m 청jo");
         input.add("마음이 라음 녜 났 나예 와앤 와인");
-        List<Geul> result = input.stream()
-            .flatMap(in -> {
-                List<Character> chars = in.chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toList());
-                chars.add('\n');
-                return chars.stream();
-            })
-            .map(c -> GeulTypeResolver.resolve(c).create(c))
-            .toList();
+        input.add("감 겳 운");
+        input.add("123-1운동");
 
-        // String input = "asd";
-        // List<Geul> result = input.chars().mapToObj(c -> (char) c)
-        //     .map(c -> GeulTypeResolver.resolve(c).create(c))
-        //     .toList();
-
-        List<BrailleToken> jungja = result.stream()
-            .map(geul -> {
-                return jungjaMapper.get(geul.getClass()).uncontracted(geul);
-            })
-            .toList();
-        List<BrailleToken> yakja = result.stream()
-            .map(geul -> {
-                return yakjaMapper.get(geul.getClass()).contraction(geul);
-            }).toList();
-        List<BrailleToken> rule = engine.process(yakja);
-        String output = 
-            // jungja
-            rule
-            .stream()
-            .map(BrailleToken::toString)
-            .map(s -> s.replace("\0", ""))
-            .collect(Collectors.joining());
+        String output = BRFEngine.toBRF(
+            engine.process(
+                input.stream().flatMap(in -> 
+                    (in + "\n").chars().mapToObj(c -> (char) c))
+                .map(c -> GeulTypeResolver.resolve(c).create(c))
+                .map(geul -> {
+                    // return jungjaMapper.get(geul.getClass()).uncontracted(geul);
+                    return yakjaMapper.get(geul.getClass()).contraction(geul);
+                })
+                .toList()
+        ));
             
         try (Writer writer = new BufferedWriter(
-            new OutputStreamWriter(new FileOutputStream("java/demo/target/ouput.txt"), StandardCharsets.UTF_8)
+            new OutputStreamWriter(new FileOutputStream("java/demo/target/ouput.brf"), StandardCharsets.UTF_8)
         )) {
             writer.write(output);
         } catch (FileNotFoundException e) {
