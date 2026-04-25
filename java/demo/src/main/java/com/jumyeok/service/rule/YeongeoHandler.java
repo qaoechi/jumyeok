9e89243df16cdac34671e15b35fb7ad0c14cfafa
qaoechi <junseong10683@gmail.com>
2026-04-25 14:36:48 +0900

    feat: Improve uppercase handling and english start end logic

--- java/demo/src/main/java/com/jumyeok/service/rule/YeongeoHandler.java
+++ java/demo/src/main/java/com/jumyeok/service/rule/YeongeoHandler.java
@@ -6,14 +6,28 @@ import com.jumyeok.constants.rule.BrailleHelper;
 import com.jumyeok.model.braille.BrailleToken;
 import com.jumyeok.model.braille.Segment;
 import com.jumyeok.model.braille.TokenType;
+import com.jumyeok.model.mukja.Yeongeo;
 
 public class YeongeoHandler implements TransitionRule {
     @Override
     public void handle(Segment current, Segment prev, Segment next, List<BrailleToken> result) {
-        result.add(new BrailleToken(BrailleHelper.ROMAJAPYO, TokenType.HELPER, null, false));
-        result.addAll(current.getTokens());
-        if (next != null && !(next.getType() == TokenType.GONGBAEK && next.getTokens().get(0).getOrigin().getChar() == '\n')) {
-            result.add(new BrailleToken(BrailleHelper.ROMAJONGRYO, TokenType.HELPER, null, false));
+        if (prev.getType() != TokenType.YEONGEO) result.add(BrailleHelper.ROMAJAPYO);
+
+        List<BrailleToken> tokens = current.getTokens();
+        for (int i = 0; i < tokens.size(); i++) {
+            boolean curr = ((Yeongeo)tokens.get(i).getOrigin()).isUpper();
+            boolean pre = i > 0 ? ((Yeongeo)tokens.get(i - 1).getOrigin()).isUpper() : false;
+            Yeongeo nex = i < tokens.size() - 1 ? ((Yeongeo)tokens.get(i + 1).getOrigin()) : null;
+
+            if (curr && !pre && (nex != null && nex.isUpper())) result.add(BrailleHelper.DAEMUNGIHO);
+            if (curr && !pre && (nex != null && !nex.isUpper())) result.add(BrailleHelper.DAEMUNJA);
+            result.add(tokens.get(i));
+            if (curr && pre && (nex != null && !nex.isUpper())) result.add(BrailleHelper.DAEMUNJONGRYO);
         }
+        if (next != null && (next.getType() == TokenType.SUTJA)) return;
+        // if (next != null && (next.getType() == TokenType.GONGBAEK && next.getTokens().get(0).getOrigin().getChar() == '\n')) {
+        //     return;
+        // }
+        if (next.getType() != TokenType.YEONGEO) result.add(BrailleHelper.ROMAJONGRYO);
     }
 }
